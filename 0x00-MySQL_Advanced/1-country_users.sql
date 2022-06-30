a896daae6f2b51fcb382b981c1672a0b6656e51c
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-06-30 19:51:27 +0300

    :memo: In and not out

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -4,4 +4,4 @@ CREATE TABLE IF NOT EXISTS users (
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255),
     country CHAR(2) NOT NULL DEFAULT 'US' CHECK (country IN ('US', 'CO', 'TN'))
-);
+)ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
