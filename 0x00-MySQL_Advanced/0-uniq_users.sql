f8894aee6d39716b91df9d40dbe14175bca9847c
Hezekiah-Elisha <elishahezekiah903@gmail.com>
2022-06-30 18:18:06 +0300

    :memo: We are all unique

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,4 +1,4 @@
---create an SQL script that creates a table called users
+--create a table called users
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
